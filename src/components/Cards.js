36ba9893870a21e6209d1e09c2b273cb9db1ab49
amandayu1 <amanda_yu2002@live.com>
2020-12-31 02:50:34 -0500

    debugged cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Cards.css';
 
 function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
-		<a target="_blank" href={props.path} rel="noreferrer">
+		<a target="_blank" href={props.path} className="cards__item__link">
           <figure className='cards__item__pic-wrap' data-category={props.label}>
             <img
               className='cards__item__img'
@@ -16,7 +17,7 @@ function CardItem(props) {
           <div className='cards__item__info'>
             <h5 className='cards__item__text'>{props.text}</h5>
           </div>
-		</a> 
+		</a>
       </li>
     </>
   );
